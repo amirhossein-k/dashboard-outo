e4780eb075b5ecdde88ddd21196bdfe8d7dfb42e
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2022-12-16 13:57:35 +0000

    Update 15 files

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss";
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <h1 className="title">Outo Amir</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
