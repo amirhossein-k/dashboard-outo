33e3b8bf689b4d711f5cbf3382f95ad0a6190b5f
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2022-12-25 12:37:17 +0000

    Update 4 files

--- src/Pages/Single/Single.js
+++ src/Pages/Single/Single.js
@@ -1,45 +1,74 @@
 import "./single.scss";
-import {Col} from 'react-bootstrap'
-import Sidebar from '../../components/sidebar/Sidebar'
-
+import { Col, Row } from "react-bootstrap";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Chart from "../../components/chart/Chart";
+import Tables from "../../components/tables/Tables";
 
 const Single = () => {
   return (
-    <div className="single  row">
-      
-      <Col  sm={12} md={2} lg={1} className='fixlistnavbar'><Sidebar/></Col>
-      <Col  sm={12} md={10} lg={11}>
-        <div className="singleContainer">
-          <div className="top">
-            <div className="left">
+    <Row className="single">
+      {/* /////////////////////////////  */}
+      <Col sm={12} md={2} lg={1} className="fixlistnavbar">
+        <Sidebar />
+      </Col>
+      {/* ////////////////////////////////// */}
+      <Col sm={12} md={10} lg={11}>
+        <Row className="singleContainer">
+          <Col className="top" xs={12} md={4} lg={4}>
+            <Row className="left">
               <div className="editButton">Edit </div>
-              <h1 className="title">Information</h1>
-              <div className="item">
-                <img src="https://res.cloudinary.com/dijamrzud/image/upload/v1668962724/cars2_tgwa6d.jpg" 
-                alt="" className="itemImg"/>
+              <Col xs={12} ><h1 className="title">Information</h1></Col>
+              {/* ///item */}
+              <Col className="item" xs={12}>
+                <img
+                  src="https://res.cloudinary.com/dijamrzud/image/upload/v1668962724/cars2_tgwa6d.jpg"
+                  alt=""
+                  className="itemImg"
+                />
+
+                {/* ///details */}
                 <div className="details">
                   <h1 className="itemTitle">samand</h1>
-                  <div className="dataItem">
-                    <span className="itemKey">کارخانه</span>
+                  <div className="detailItem">
+                    <span className="itemKey">کارخانه:</span>
                     <span className="itemValue">سایپا</span>
                   </div>
-                  <div className="dataItem">
-                    <span className="itemKey">کارکرد</span>
+                  <div className="detailItem">
+                    <span className="itemKey">کارکرد:</span>
                     <span className="itemValue">5</span>
                   </div>
-                  <div className="dataItem">
-                    <span className="itemKey">وضعیت</span>
+                  <div className="detailItem">
+                    <span className="itemKey">وضعیت:</span>
                     <span className="itemValue">موجود</span>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="right"></div>
-          </div>
-          <div className="buttom"></div>
-        </div>
+                {/* ////details end */}
+              </Col>
+              {/* ///item end */}
+            </Row>
+            {/* //// left end */}
+          </Col>
+          {/* /// top col end */}
+          {/* /////////////////////// */}
+          <Col className="right" md={8} lg={8} style={{paddingBlock: 20}}>
+            <Chart aspect={3 / 1} title={'نوسانات'}/>
+          </Col>
+          {/* ////////////////////// */}
+
+          <Col className="buttom" xs={11}>
+            <Row>
+              <Col xs={12}><h1 className="title">Information</h1></Col>
+              <Col>
+                <Tables/>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        {/* //row single container */}
       </Col>
-    </div>
+      {/* // /col end */}
+    </Row>
+    // / row single end
   );
 };
 
