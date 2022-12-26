c803e59578f2edf7a1a0b90cd4729a88b633c617
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2022-12-26 16:05:02 +0000

    Update 3 files

--- src/Pages/New/New.js
+++ src/Pages/New/New.js
@@ -1,10 +1,38 @@
+import {useState} from 'react'
 import "./new.scss";
+import { Col, Row,Button,Form } from "react-bootstrap";
+
+import Sidebar from "../../components/sidebar/Sidebar";
 const New = () => {
+  const [namecar,setNameCar] = useState('')
   return (
-    <>
-      <h1>New</h1>
-    </>
-  );
+   <Row className="new">
+     {/* ///////Sidebar//////// */}
+     <Col sm={12} md={2} lg={1}  className="fixlistnavbar">
+       <Sidebar/>
+      </Col>
+      {/* /////////Container//////// */}
+      <Col className="newContainer">
+        <div className="top">
+          <h1>Add New Product</h1>
+        </div>
+        {/* ///// end top ////// */}
+        <div className="bottom-new">
+          <Form className="formfix">
+            <Form.Group controlId="title">
+              <Form.Label>نام خودرو</Form.Label>
+              <Form.Control type="title" value={namecar}
+                placeholder="نام خودرو"
+                onChange={e=> setNameCar(e.target.value)} />
+            </Form.Group>
+          </Form>
+        </div>
+        {/* ///// end bottom ///// */}
+      </Col>
+     
+   
+   </Row>
+  )
 };
 
 export default New;
